6443358af7b0fe6201d3ac424b1e10e28a1b13a8
TheXanada96_MYCUS <85071995+TheXanada96@users.noreply.github.com>
2024-05-29 13:19:36 +0200

    roBBA AGGIUNTA

--- Platform 2D/Player.gd
+++ Platform 2D/Player.gd
@@ -1,4 +1,23 @@
 extends KinematicBody2D
 
+var motion = Vector2(0,0); var Obama = 2
+const SPEED = 500
+
 func _physics_process(delta):
 	pass
+	if Input.is_action_pressed("ui_left")\
+	and not Input.is_action_pressed("ui_right"):
+		motion.x = -SPEED
+		
+	elif Input.is_action_pressed("ui_right")\
+	and not Input.is_action_pressed("ui_left"):
+		motion.x = SPEED
+		
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = SPEED
+	else:
+		motion.x = 0
+		motion.y = 0
+	move_and_slide(motion)
