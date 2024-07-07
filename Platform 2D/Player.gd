7945934696125656209d53fc1dcdac4433879577
TheXanada96_MYCUS <85071995+TheXanada96@users.noreply.github.com>
2024-07-07 20:23:52 +0200

    script sistemati

--- Platform 2D/Player.gd
+++ Platform 2D/Player.gd
@@ -1,10 +1,34 @@
 extends KinematicBody2D
 
 var motion = Vector2(0,0); var Obama = 2
-const SPEED = 500
+
+const SPEED = 1000
+const GRAVITY = 50
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 1000
 
 func _physics_process(delta):
-	pass
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP)
+
+
+func apply_gravity():
+	if not is_on_floor():
+		motion.y += GRAVITY
+	else:
+		motion.y = 0
+	
+
+
+func jump():
+	if Input.is_action_pressed("Jump") and is_on_floor() :
+		motion.y -= JUMP_SPEED
+
+
+func move():
 	if Input.is_action_pressed("ui_left")\
 	and not Input.is_action_pressed("ui_right"):
 		motion.x = -SPEED
@@ -12,12 +36,17 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("ui_right")\
 	and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
-		
-	elif Input.is_action_pressed("ui_up"):
-		motion.y = -SPEED
-	elif Input.is_action_pressed("ui_down"):
-		motion.y = SPEED
 	else:
 		motion.x = 0
-		motion.y = 0
-	move_and_slide(motion)
+
+func animate():
+	if motion.y <0:
+		$AnimatedSprite.play("jump")
+	elif motion.x >= 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
+	elif motion.x <= 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idle")
